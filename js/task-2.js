32a36b4551e9f49244befe6be02d3cb9a47e2a15
a.viazovskyi <sentynel2014@gmail.com>
2026-02-18 00:19:54 +0100

    fixed method makeArray

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,5 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  if (maxLength < 1) {
-    return []
-  }
-
-  const arrayLengthSum = firstArray.length + secondArray.length
-
-  if (arrayLengthSum <= maxLength) {
-    return firstArray.concat(secondArray)
-  } else {
-    const secondArrayMaxLength = maxLength - firstArray.length
-    return firstArray.concat(secondArray.slice(0, secondArrayMaxLength))
-  }
+  return firstArray.concat(secondArray).slice(0, maxLength)
 }
 
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)) // ["Mango", "Poly", "Ajax"]
@@ -18,4 +7,4 @@ console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)) // 
 console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)) // ["Mango", "Ajax", "Chelsea"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)) // ["Earth", "Jupiter"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)) // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)) // []
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)) // []git
